3d561453acdac025085c5f79cd9df3233992f920
Manish choudhary <143925791+Manish102002@users.noreply.github.com>
2024-02-21 18:24:17 +0530

    Update LandingPage.jsx
    fix landing video

--- src/Components/Hero/LandingPage.jsx
+++ src/Components/Hero/LandingPage.jsx
@@ -4,6 +4,7 @@ import Footer from "../Footer/Footer"
 import NextPage from "./NextPage"
 import React, { useEffect, useRef } from "react";
 import { gsap } from "gsap";
+import bikeV from '/public/vedio/vedio.mp4'
 // import { Power0 } from "gsap-trial/all";
 // import { ScrollTrigger } from "gsap/ScrollTrigger";
 
@@ -16,7 +17,7 @@ const LandingPage = () => {
             <div>
                 <div className="background-video-container lg:h-[588px] md:h-[458px]">
                     <video autoPlay muted loop className="background-video " >
-                        <source src="/public/vedio/vedio.mp4" type="video/mp4" />
+                        <source src={bikeV} type="video/mp4" />
 
                     </video>
                    
