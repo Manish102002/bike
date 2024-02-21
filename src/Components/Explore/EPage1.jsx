1aa092f91de6ff3fb61cf05c49c1d7f8dc93a91b
Manish <i.m.manish100202@gmail.com>
2024-02-21 20:14:13 +0530

    vedio

--- src/Components/Explore/EPage1.jsx
+++ src/Components/Explore/EPage1.jsx
@@ -2,6 +2,7 @@ import Footer from "../Footer/Footer"
 import Navi from "../Header/Navi"
 import EPage2 from "./EPage2"
 import EPage3 from "./EPage3"
+import bike2 from '/public/vedio/evedio.mp4'
 // import EPage4 from "./EPage4"
 
 
@@ -12,7 +13,7 @@ const EPage1 = () => {
       <div>
         <div className="background-video-container lg:h-[588px] md:h-[458px]">
           <video autoPlay muted loop className="background-video " >
-            <source src="/public/vedio/evedio.mp4" type="video/mp4" />
+            <source src={bike2} type="video/mp4" />
           </video>
         </div>
       </div>
